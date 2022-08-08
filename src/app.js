1c4794c91722fec7337b928a02bc90a4e3cb5d4f
KC-Morrison <102931217+KC-Morrison@users.noreply.github.com>
2022-08-08 18:43:43 +1000

    Showing current conditions
    Added JS to update temperature, city name, humidity, wind speed and weather description

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "8ccf37f47c78fce7cbde0d0a29369196";
+let city = "Sydney";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
